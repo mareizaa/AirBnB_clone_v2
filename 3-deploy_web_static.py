45e78d4465e3dfd2a57f07fd1ad882cb346e524d
mareizaa <marce.3210@gmail.com>
2021-08-27 02:43:22 +0000

    task3

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''Generates a .tgz archive from the contents of the web_static'''
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    '''Generates a .tgz archive from the contents of the web_static'''
+    try:
+        name = datetime.now().strftime('%Y%m%d%H%M%S')
+        name_file = "versions/web_static_{}.tgz".format(name)
+        local('mkdir -p versions')
+        print('Packing web_static to {}'.format(name_file))
+        local('tar -cvzf {} web_static'.format(name_file))
+    except:
+        None
+
+def do_deploy(archive_path):
+    '''
+    Distributes an archive to your web servers,
+    using the function do_deploy
+    '''
+    put('')
