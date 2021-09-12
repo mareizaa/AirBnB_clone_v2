28a01fd70a703aef86c6a244fa114b646ec8378b
mareizaa <marce.3210@gmail.com>
2021-09-12 21:46:36 +0000

    task4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" starts a Flask web application """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def hbnb_text(text):
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python/', strict_slashes=False, defaults={'text': "is cool"})
+@app.route('/python/<text>')
+def python(text):
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def is_number(n):
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
