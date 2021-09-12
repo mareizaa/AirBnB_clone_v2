478c794132f968f55371e61fdbde1ce79ea5f7bc
mareizaa <marce.3210@gmail.com>
2021-09-12 22:41:32 +0000

    task 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -35,7 +35,7 @@ def render(n):
     return render_template('5-number.html', n=n)
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def render_t(n):
     return render_template('6-number_odd_or_even.html', n=n)
 
